29a4904c1214244a1ca1a7dc5abddd1bba0c8873
EL.MLIKI.HICHAM <elmliki.hicham@gmail.com>
2026-01-07 14:08:42 +0100

    Inhiratence

---
+++ Module 2 - Object Oriented Programming/Inheritance and Polymorphism/Main.java
@@ -0,0 +1,13 @@
+import product.Shirt;
+import product.Shirt.Size;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Shirt shirt = new Shirt(); 
+        shirt.setPrice(500);
+        shirt.setSize(Size.MEDIUM);
+
+    }
+}
